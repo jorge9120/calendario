2c6c5d454ec218c6df72815685616755584b7729
Jorge Andres Sanchez Carrillo <jasanchez@personalsoft.com.co>
2020-06-25 15:21:07 -0500

    cargando calendario

---
+++ projects/demos/app/demo-app.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  NgbTabsetModule,
+  NgbCollapseModule,
+  NgbTooltipModule,
+} from '@ng-bootstrap/ng-bootstrap';
+import { Angulartics2Module } from 'angulartics2';
+import { DragAndDropModule } from 'angular-draggable-droppable';
+import { DemoAppComponent } from './demo-app.component';
+import { DemoComponent as DefaultDemoComponent } from './demo-modules/kitchen-sink/component';
+import { DemoModule as DefaultDemoModule } from './demo-modules/kitchen-sink/module';
+import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
+import { ClipboardModule } from 'ngx-clipboard';
+
+@NgModule({
+  declarations: [DemoAppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    NgbTabsetModule,
+    NgbCollapseModule,
+    NgbTooltipModule,
+    DragAndDropModule,
+    Angulartics2Module.forRoot({
+      developerMode: !environment.production,
+    }),
+    ClipboardModule,
+    DefaultDemoModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: '',
+          component: DefaultDemoComponent,
+
+        },
+        {
+          path: '**',
+          redirectTo: 'kitchen-sink',
+        },
+      ],
+      {
+        useHash: true,
+      }
+    ),
+  ],
+  bootstrap: [DemoAppComponent],
+})
+export class DemoAppModule {}
